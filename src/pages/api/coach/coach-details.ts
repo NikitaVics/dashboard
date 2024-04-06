6e7b8e7676a88aa866e16aecee996e13ad363454
SaravanaAnish <saravana@verdant.co.in>
2024-04-06 20:49:09 +0530

    fixed coach details issue

--- src/pages/api/coach/coach-details.ts
+++ src/pages/api/coach/coach-details.ts
@@ -19,7 +19,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       res.status(200).json(response)
     } catch (error) {
       console.log(error)
-      res.status(403).end()
+      res.status(500).end()
     }
   }
 }
