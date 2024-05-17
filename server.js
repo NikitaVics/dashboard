377ec188b6d9c732abb5df5a51c6143c022e1db5
NikitaVics <161066857+NikitaVics@users.noreply.github.com>
2024-05-17 10:15:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-// server.js
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const port = process.env.PORT || 3010;
+const port = process.env.PORT || 3002;
 const app = next({ dev })
 const handle = app.getRequestHandler()
-console.log("ooooooooooooooooo");
+
 app.prepare().then(() => {
   createServer((req, res) => {
-    // Be sure to pass true as the second argument to url.parse.
+    // Be sure to pass `true` as the second argument to `url.parse`.
     // This tells it to parse the query portion of the URL.
     const parsedUrl = parse(req.url, true)
     const { pathname, query } = parsedUrl
@@ -24,6 +23,6 @@ app.prepare().then(() => {
     }
   }).listen(port, (err) => {
     if (err) throw err
-    console.log(`Ready UI on  http://localhost:${port}`)
-  })
+    console.log(`> Ready on http://localhost:${port}`)
+  })
 })
